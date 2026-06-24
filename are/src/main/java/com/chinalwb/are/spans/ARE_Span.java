c583c529cbf73f3c72d2b43eeabc99a38be8a100
Ameer Muawiya <ameermuavia604@gmail.com>
2026-06-24 13:59:43 +0500

    Delete are directory

--- are/src/main/java/com/chinalwb/are/spans/ARE_Span.java
+++
@@ -1,9 +0,0 @@
-package com.chinalwb.are.spans;
-
-/**
- * Created by wliu on 2018/2/11.
- */
-
-public interface ARE_Span {
-    public String getHtml();
-}
--- are/src/main/java/com/chinalwb/are/spans/AreUnderlineSpan.java
+++
@@ -1,7 +0,0 @@
-package com.chinalwb.are.spans;
-
-import android.text.style.UnderlineSpan;
-
-public class AreUnderlineSpan extends UnderlineSpan {
-
-}
